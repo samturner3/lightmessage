3309c59e36f292b5576f68916ecd293a5bc51f96
Rachel Wilson <rachel.wilson@theodi.com>
2019-05-27 22:31:02 +0100

    Adds form for test messages

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function (req, res, next) {
-    res.send('i\'m here for you');
+    res.render('index');
 });
 
 router.post('/', function (req, res, next) {
