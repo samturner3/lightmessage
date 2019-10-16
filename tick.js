202e1f4bda6330725a69361f5dea5e1e9eb41448
samturner3 <samburner3@gmail.com>
2019-10-16 17:39:15 +1100

    fonts

--- tick.js
+++ tick.js
@@ -21,25 +21,59 @@ let tickLux
 let scrollMessage = 'hello world'
 let newMessage = true;
 
-let clockWidth = 72
+const screenWidth = 32*4
+
+let clockWidth = 7*6
 const widthSpacing = 10
 
-const font1 = __dirname + '/fonts/' + '9x15.bdf'
-const font2 = __dirname + '/fonts/' + '6x12.bdf'
-const font3 = __dirname + '/fonts/' + '6x10.bdf'
+const fonts = [
+__dirname + '/fonts/' + '4x6.bdf', //0
+__dirname + '/fonts/' + '5x7.bdf', //1
+__dirname + '/fonts/' + '5x8.bdf', //2
+__dirname + '/fonts/' + '6x9.bdf', //3
+__dirname + '/fonts/' + '6x10.bdf', //4
+__dirname + '/fonts/' + '6x12.bdf', //5
+__dirname + '/fonts/' + '6x13.bdf', //6
+__dirname + '/fonts/' + '6x13B.bdf', //7
+__dirname + '/fonts/' + '6x13O.bdf', //8
+__dirname + '/fonts/' + '7x13.bdf', //9
+__dirname + '/fonts/' + '7x13B.bdf', //10
+__dirname + '/fonts/' + '7x13O.bdf', //11
+__dirname + '/fonts/' + '7x14.bdf', //12
+__dirname + '/fonts/' + '7x14B.bdf', //13
+__dirname + '/fonts/' + '8x13.bdf', //14
+__dirname + '/fonts/' + '8x13B.bdf', //15
+__dirname + '/fonts/' + '8x13O.bdf', //16
+__dirname + '/fonts/' + '9x15.bdf', //17
+__dirname + '/fonts/' + '9x15B.bdf', //18
+__dirname + '/fonts/' + '9x18.bdf', //19
+__dirname + '/fonts/' + '9x18B.bdf', //20
+__dirname + '/fonts/' + '10x20.bdf', //21
+__dirname + '/fonts/' + 'cIR6x12.bdf', //22
+__dirname + '/fonts/' + 'helvR12.bdf', //23
+__dirname + '/fonts/' + 'tom-thumb.bdf', //24
+]
+
+const getFontDimentions = function(fontIndex) {
+  const x = fonts[fontIndex].split('/fonts/')[1].split('x')[0].replace(/\D/g,'');
+  const y = fonts[fontIndex].split('/fonts/')[1].split('x')[1].replace(/\D/g,'');
+  // console.log('x:', x, ' y:',y)
+  return({x,y})
+}
+
 
 const drawStaticMessages = function () {
   if (globalMode.luxAuto){
     brightnessChangeLux(tickLux)
   }
-  if (tickTime) globalMode.led.drawText(0, 0, tickTime, font1, 255, 0, 0)
+  if (tickTime) globalMode.led.drawText(0, 0, tickTime, fonts[15], 255, 0, 0)
   else clockWidth = 0
-  if (tickTemp) globalMode.led.drawText((clockWidth + (tickTime ? widthSpacing : 0)), 2, tickTemp, font2, 0, 255, 255)
+  if (tickTemp) globalMode.led.drawText((screenWidth - (tickTemp.length * getFontDimentions(14).x)), 0, tickTemp, fonts[14], 0, 0, 255)
   if (globalMode.tick.lux) {
     if (tickLux) {
-      globalMode.led.drawText(0, 10, tickLux.toString(), font2, 0, 255, 0)
+      globalMode.led.drawText(0, 10, tickLux.toString(), fonts[9], 0, 255, 0)
     } else {
-      globalMode.led.drawText(0, 10, 'no lux data', font2, 0, 255, 0)
+      globalMode.led.drawText(0, 10, 'no lux data', fonts[9], 0, 255, 0)
     }
   }
 }
@@ -110,7 +144,7 @@ const updateLux = async function () {
   }, 1000)
 }
 
-const scrollAMessage = async function (message, speed = 5, loops, y = 0, includeStaticMessages = true) {
+const scrollAMessage = async function (message, speed = 5, loops, y = 12, includeStaticMessages = true) {
 
   var startX = (32*4);
   var endX = Math.abs(message.length * 6) * -1;
@@ -128,7 +162,7 @@ const scrollAMessage = async function (message, speed = 5, loops, y = 0, include
     for (let x = startX; x > endX ; x--) {        
         // console.log('runn', x)
         globalMode.led.clear()
-        globalMode.led.drawText(x, y, message, font2, 0, 255, 0)
+        globalMode.led.drawText(x, y, message, fonts[3], 0, 255, 0)
         if (includeStaticMessages) {
           drawStaticMessages();
         }
