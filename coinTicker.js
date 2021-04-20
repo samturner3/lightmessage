c4be0efd879a21f28bf2271789ee62d747452b80
samturner3 <samburner3@gmail.com>
2021-04-20 11:55:02 +1000

    add formatting

--- coinTicker.js
+++ coinTicker.js
@@ -3,15 +3,13 @@ const coinTicker = require('coin-ticker');
 module.exports = function coinTickerLoop() {
   setTimeout(async () => {
     // console.log('time updated'){
-    let usd;
     coinTicker('poloniex', 'DOGE_USD')
       .then((pairs) => {
         console.log(pairs);
-        usd = pairs.last;
+        globalMode.tick.values.coinTicker = `1 DOGE = ${pairs.last} USD = ${3163*pairs.last}`;
       });
 
-    globalMode.tick.values.coinTicker = `1 DOGE = ${usd}`;
     if (!globalMode.tick.coinTicker || globalMode.brightness === 1) globalMode.tick.values.coinTicker = undefined;
     coinTickerLoop();
-  }, 60000);
+  }, 5000);
 };
